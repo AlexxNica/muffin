3584e38158f813134551ad34f73870dd3851cba1
Unknown <joakim.lindqvist@frostbite.com>
2016-12-12 11:02:14 +0100

    Fixed pep8

--- muffin/v2/__init__.py
+++ muffin/v2/__init__.py
@@ -4,9 +4,11 @@ import flask
 
 hello_blueprint = flask.Blueprint("hello", __name__)
 
+
 @hello_blueprint.route('/')
 def hello():
     return flask.jsonify({"hello": "world"})
+
 
 def register_api(app, url_prefix):
     app.register_blueprint(hello_blueprint, url_prefix=url_prefix)
