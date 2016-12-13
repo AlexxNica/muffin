6b8bc7f2cd6809cfb89f093efe2980c24df5998b
Jake <jake.shadle@frostbite.com>
2016-12-13 11:24:51 +0100

    docs: Fix gh-pages upload

--- docs/upload.js
+++ docs/upload.js
@@ -2,6 +2,10 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('target', {
 	message: 'Auto-generated docs',
+	user: {
+		name: 'Jake Shadle',
+		email: 'jake.shadle@frostbite.com'
+	},
 	logger: function(message) {
 		console.log(message);
 	}
