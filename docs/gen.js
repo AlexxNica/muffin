cf6f47412fe60537a2ac3ee1a7a7617632ab3bef
Jake <jake.shadle@frostbite.com>
2016-12-13 11:39:52 +0100

    fix: Copy css/ to target

--- docs/gen.js
+++ docs/gen.js
@@ -1,15 +1,16 @@
 const bp = require('bootprint');
-const fs = require('fs-extra')
+const fs = require('fs-extra');
 const swagger = require('bootprint-swagger');
 
 // Create our target directory where all generated content is stored
 // and uploaded
 if (!fs.existsSync('target')) {
-	fs.mkdirSync('target')
+	fs.mkdirSync('target');
 }
 
 // Move our root index
-fs.copySync('./docs/index.html', 'target/index.html')
+fs.copySync('./docs/index.html', 'target/index.html');
+fs.copySync('./docs/css', 'target/css');
 
 // Generate the API documentation from the swagger file
 var bootprint = bp
