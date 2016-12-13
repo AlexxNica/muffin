e565ff781ce802e5795984ff65b32ba58aea6923
Jake <jake.shadle@frostbite.com>
2016-12-13 11:34:50 +0100

    fix: Add missing comma

--- docs/upload.js
+++ docs/upload.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('target', {
-	repo: 'https://' + process.env.GH_TOKEN + '@github.com/patissiere/muffin.git'
+	repo: 'https://' + process.env.GH_TOKEN + '@github.com/patissiere/muffin.git',
 	message: 'Auto-generated docs',
 	user: {
 		name: 'Jake Shadle',
